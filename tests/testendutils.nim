854b50128802b378559f4226f51dd26ac4aba245
Dylan Hackworth <dylhack@tutanota.com>
2021-09-24 13:59:33 -0500

    added tests/testsendutils

---
+++ tests/testendutils.nim
@@ -0,0 +1,24 @@
+import unittest
+import strformat
+import "nimtrix/endutils"
+import "nimtrix/client/endpoints"
+
+const server = "https://newcircuit.io"
+
+func expect(target: string): string =
+  return fmt"{server}{target}"
+
+test "can build plain endpoint":
+  const expected = expect loginSubmit.path
+  let endpoint = loginSubmit.build server
+  check ($endpoint == expected)
+
+test "can build param based endpoint":
+  const expected = expect(
+    "/_matrix/client/r0/user/%40dylhack%3Anewcircuit.io/filter/1"
+  )
+  let endpoint = filterGet.build(
+    server, 
+    ("userId", "@dylhack:newcircuit.io"), ("filterId", "1")
+  )
+  check ($endpoint == expected)
