3622083552611f8161c641970782b6e22e0bdbac
Dylan Hackworth <dylhack@tutanota.com>
2021-09-25 01:52:11 -0500

    Added UnknownError

--- src/matrix/core/errors.nim
+++ src/matrix/core/errors.nim
@@ -4,10 +4,14 @@ type MatrixError* {.pure.} = ref object of CatchableError
   errcode*: string
   error*: string
 
+type UnknownError* {.pure.} = ref object of CatchableError
+  body*: string
+
 proc buildMxError*(body: string): MatrixError =
   var data = parseJson(body)
   if not data.contains("error") or not data.contains("errcode"):
-    raise newException(CatchableError, body)
+    let unknown = UnknownError(body: body)
+    raise unknown
   var message = data["error"].getStr()
   var code = data["errcode"].getStr()
   return MatrixError(errcode: code, error: message)
