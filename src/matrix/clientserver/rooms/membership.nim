c11e7e764644a82fae3152aa7952383dc1a3cf4a
tandy <tandy1000@cock.li>
2022-01-26 17:46:15 +0000

    add join room endpoint

--- src/matrix/clientserver/rooms/membership.nim
+++ src/matrix/clientserver/rooms/membership.nim
@@ -1,4 +1,3 @@
-import std/httpcore
 import pkg/jsony
 import ../../core
 import ../endpoints
@@ -7,9 +6,26 @@ import ../../asyncutils
 type
   JoinedRoomsRes* = object
     joinedRooms*: seq[string]
+  JoinRoomReq* = object
+    roomId*: string
+  JoinRoomRes* = object
+    roomId*: string
 
 proc newJoinedRoomsRes(res: PureResponse): JoinedRoomsRes =
   return res.body.fromJson(JoinedRoomsRes)
+
+proc newJoinRoomReq(
+  client: MatrixClient,
+  roomId: string,
+  ): PureRequest =
+  let target = roomJoinId.build(client.server, urlParams = [("roomId", roomId)])
+  return PureRequest(
+    endpoint: target,
+    data: ""
+  )
+
+proc newJoinRoomRes(res: PureResponse): JoinRoomRes =
+  return res.body.fromJson(JoinRoomRes)
 
 proc joinedRooms*(
   client: MatrixClient,
@@ -22,3 +38,15 @@ proc joinedRooms*(
     )
     res = await client.request(req)
   return newJoinedRoomsRes(res)
+
+proc joinRoom*(
+  client: MatrixClient,
+  roomId: string,
+): Future[JoinRoomRes] {.fastsync.} =
+  let
+    req = newJoinRoomReq(
+      client,
+      roomId
+    )
+    res = await client.request(req)
+  return newJoinRoomRes(res)
