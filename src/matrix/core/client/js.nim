6c0073ed3ecb1959cf77ca8cc729a2d1afa9abff
tandy <tandy1000@cock.li>
2022-01-29 13:00:51 +0000

    refactor to use experimental jshttpclient

--- src/matrix/core/client/js.nim
+++ src/matrix/core/client/js.nim
@@ -1,118 +1,28 @@
 ## This is the frontend JavaScript iteration of the MatrixClient. This module
-## should match up with native.nim and have the same publicly accessible 
+## should match up with native.nim and have the same publicly accessible
 ## procedures and types.
-import std/[dom, strformat,
-  jsfetch, uri, jsheaders,
-  jsffi, httpcore, macros]
-import jsony
-import pure
-import ../endutils
-import ../../asyncutils
+import
+  std/[dom, strformat, uri, jsheaders, httpcore],
+  pkg/jsony,
+  pkg/nodejs/jsmultisync,
+  pkg/nodejs/experimental/jshttpclient
+  pure,
+  ../endutils,
+  ../../asyncutils
 
 type
-  JsHttpClient* = object
-    ## This is our own custom implementation of an HttpClient. This exists
-    ## because both AsyncHttpClient and HttpClient both have a headers
-    ## property, so this brings consistency since there isn't an
-    ## HttpClient for JavaScript.
-    headers*: Headers
-  JsMatrixClient* = MatrixClient[JsHttpClient]
-  # HACK(dylhack): AsyncMatrixClient and SyncMatrixClient allows us to use
-  # the multisync pragma everywhere despite JavaScript only accepting async
-  # code.
-  AsyncMatrixClient* = JsMatrixClient
-  SyncMatrixClient* = JsMatrixClient
+  AsyncMatrixClient* = MatrixClient[JsAsyncHttpClient]
+  SyncMatrixClient* = MatrixClient[JsHttpClient]
 
-MatrixClient.setSync(SyncMatrixClient)
 MatrixClient.setAsync(AsyncMatrixClient)
+MatrixClient.setSync(SyncMatrixClient)
 
-proc replaceReturn(node: var NimNode) =
-  var z = 0
-  for s in node:
-    var son = node[z]
-    let jsResolve = ident("jsResolve")
-    if son.kind == nnkReturnStmt:
-      let value = if son[0].kind != nnkEmpty: nnkCall.newTree(jsResolve, son[
-          0]) else: jsResolve
-      node[z] = nnkReturnStmt.newTree(value)
-    elif son.kind == nnkAsgn and son[0].kind == nnkIdent and $son[0] == "result":
-      node[z] = nnkAsgn.newTree(son[0], nnkCall.newTree(jsResolve, son[1]))
-    else:
-      replaceReturn(son)
-    inc z
-
-proc isFutureVoid(node: NimNode): bool =
-  result = node.kind == nnkBracketExpr and
-    node[0].kind == nnkIdent and $node[0] == "Future" and
-    node[1].kind == nnkIdent and $node[1] == "void"
-
-proc generateJsasync(arg: NimNode): NimNode =
-  if arg.kind notin {nnkProcDef, nnkLambda, nnkMethodDef, nnkDo}:
-    error("Cannot transform this node kind into an async proc." &
-      " proc/method definition or lambda node expected.")
-
-  result = arg
-  var isVoid = false
-  let jsResolve = ident("jsResolve")
-  if arg.params[0].kind == nnkEmpty:
-    result.params[0] = nnkBracketExpr.newTree(ident("Future"), ident("void"))
-    isVoid = true
-  elif isFutureVoid(arg.params[0]):
-    isVoid = true
-
-  var code = result.body
-  replaceReturn(code)
-  result.body = nnkStmtList.newTree()
-
-  if len(code) > 0:
-    var awaitFunction = quote:
-      proc await[T](f: Future[T]): T {.importjs: "(await #)", used.}
-    result.body.add(awaitFunction)
-
-    var resolve: NimNode
-    if isVoid:
-      resolve = quote:
-        var `jsResolve` {.importjs: "undefined".}: Future[void]
-    else:
-      resolve = quote:
-        proc jsResolve[T](a: T): Future[T] {.importjs: "#", used.}
-        proc jsResolve[T](a: Future[T]): Future[T] {.importjs: "#", used.}
-    result.body.add(resolve)
-  else:
-    result.body = newEmptyNode()
-  for child in code:
-    result.body.add(child)
-
-  if len(code) > 0 and isVoid:
-    var voidFix = quote:
-      return `jsResolve`
-    result.body.add(voidFix)
-
-  let asyncPragma = quote:
-    {.codegenDecl: "async function $2($3)".}
-
-  result.addPragma(asyncPragma[0])
-
-proc newRequest(data: PureRequest): (Request, FetchOptions) =
-  var req = newRequest(($data.endpoint.target).cstring)
-  var fReq = newFetchOptions(
-    metod = data.endpoint.httpMethod,
-    body = data.data.cstring,
-    mode = fmCors,
-    credentials = fcOmit,
-    cache = fchNoCache,
-    referrerPolicy = frpNoReferrer,
-    keepalive = false)
-
-  return (req, fReq)
-
-proc getHttpCl(): JsHttpClient =
-  let headers = newHeaders()
-  for pair in defaultHeaders:
-    let (key, val) = pair
-    headers[key.cstring] = val.cstring
-
-  return JsHttpClient(headers: headers)
+proc newRequest(data: PureRequest): JsRequest =
+  return newJsRequest(
+    url = ($data.endpoint.target).cstring,
+    `method` = data.endpoint.httpMethod,
+    body = data.data.cstring
+  )
 
 proc setTimeoutAsync(ms: int): Future[void] =
   let promise = newPromise() do (res: proc(): void):
@@ -128,10 +38,11 @@ proc handleRateLimit(
 
   while true:
     await setTimeoutAsync(parsed.retryAfterMs)
+    var req: Request = newRequest(req)
+    req.headers = client.headers
     let
-      (req, fReq) = newRequest(req)
-      resp = await fetch(req, fReq)
-      body = await resp.text()
+      resp = await client.request(req)
+      payload = await resp.text()
 
     if not resp.ok:
       let str = $body
@@ -157,42 +68,41 @@ proc handleRateLimit(
         code: cast[HttpCode](casted),
         headers: headers)
 
-## Dummy multisync, not for practical use, but for
-## being able to use multisync for both native and
-## JavaScript code. This will only output async code.
-macro multisync*(prc: untyped): untyped =
-  result = newStmtList()
-  result.add generateJsasync(prc)
-
-## Create a new JsMatrixClient
-proc newMatrixClient*(homeserver: string): JsMatrixClient =
+## Create a new blocking MatrixClient.
+proc newMatrixClient*(homeserver: string): SyncMatrixClient =
   let server = parseUri(homeserver)
   return JsMatrixClient(
-    http: getHttpCl(),
+    http: newJsHttpClient(),
     server: server
   )
 
-## Create a new JsMatrixClient with a token already preset.
-proc newMatrixClient*(homeserver: string, token: string): JsMatrixClient =
+## Create a new MatrixClient with a token already preset.
+proc newMatrixClient*(homeserver: string, token: string): SyncMatrixClient =
   result = newMatrixClient(homeserver)
   result.setToken token
 
-## Create a new JsMatrixClient (alias for newMatrixClient(homeserver))
-proc newAsyncMatrixClient*(homeserver: string): JsMatrixClient =
-  return newMatrixClient(homeserver)
+## Create a new non-blocking MatrixClient.
+proc newAsyncMatrixClient*(homeserver: string): AsyncMatrixClient =
+  let server = parseUri(homeserver)
+  return JsMatrixClient(
+    http: newJsAsyncHttpClient(),
+    server: server
+  )
 
-## Create a new JsMatrixClient (alias for newMatrixClient(homeserver, token))
-proc newAsyncMatrixClient*(homeserver: string, token: string): JsMatrixClient =
-  return newMatrixClient(homeserver, token)
+## Create a new non-blocking MatrixClient with a token already set.
+proc newAsyncMatrixClient*(homeserver: string, token: string): AsyncMatrixClient =
+  result = newAsyncMatrixClient(homeserver)
+  result.setToken token
 
 ## This procedure performs a HTTP request.
 proc request*(
   client: JsMatrixClient,
   req: PureRequest
 ): Future[PureResponse] {.async.} =
+  var req: Request = newRequest(req)
+  req.headers = client.headers
   let
-    (reqs, fReq) = newRequest(req)
-    resp = await fetch(reqs, fReq)
+    resp = await client.request(req)
     payload = await resp.text()
 
   if not resp.ok:
